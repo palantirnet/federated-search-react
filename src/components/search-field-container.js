4fe2e2547b2a917239c8a28dc0bd303ff013a1c9
Adrian Muresan <muresan@palantir.net>
2018-06-13 16:36:59 -0500

    UMUS-119: revert file to original format, before using prettier plugin

--- src/components/search-field-container.js
+++ src/components/search-field-container.js
@@ -1,9 +1,11 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from "react";
 import cx from "classnames";
-import AnimateHeight from "react-animate-height";
+import AnimateHeight from 'react-animate-height';
+
 
 class FederatedSearchFieldContainer extends React.Component {
+
   constructor(props) {
     super(props);
 
@@ -19,45 +21,33 @@ class FederatedSearchFieldContainer extends React.Component {
   handleClick() {
     this.setState({
       expanded: !this.state.expanded
-    });
+    })
   }
 
   render() {
     const { onNewSearch } = this.props;
-    const height = this.state.expanded ? "auto" : 0;
+    const height = this.state.expanded ? 'auto' : 0;
 
     return (
       <div className="search-filters">
-        <button
-          className={cx("search-filters__trigger", {
-            "js-search-filters-open": this.state.expanded
-          })}
-          onClick={this.handleClick.bind(this)}
+        <button className={cx("search-filters__trigger", {"js-search-filters-open": this.state.expanded})} onClick={this.handleClick.bind(this)}>Filter Results</button>
+        <AnimateHeight
+            duration={450}
+            height={height}
         >
-          Filter Results
-        </button>
-        <AnimateHeight duration={450} height={height}>
           <form className="search-filters__form">
-            <section
-              className="search-accordion"
-              aria-labelledby="section-title"
-            >
+            <section className="search-accordion" aria-labelledby="section-title">
               <div className="search-filters__row">
-                <h2 className="search-filters__title" id="section-title">
-                  Filter Results
-                </h2>
+                <h2 className="search-filters__title" id="section-title">Filter Results</h2>
               </div>
 
-              <ul className="search-accordion__group">{this.props.children}</ul>
+              <ul className="search-accordion__group">
+                {this.props.children}
+              </ul>
             </section>
 
             <div className="search-filters__row">
-              <input
-                className="search-filters__reset"
-                type="button"
-                defaultValue="Clear All"
-                onClick={onNewSearch}
-              />
+              <input className="search-filters__reset" type="button" defaultValue="Clear All" onClick={onNewSearch} />
             </div>
           </form>
         </AnimateHeight>
