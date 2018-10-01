a5742f9560a97bb01c106ef3759c90f64a4f3282
Avi Schwab <schwab@palantir.net>
2018-10-01 16:30:02 -0500

    Give sitename links a key

--- src/components/results/result.js
+++ src/components/results/result.js
@@ -36,7 +36,7 @@ class FederatedResult extends React.Component {
 
       var sites = [];
       for (var i = 0; i < sitenames.length; i++) {
-        sites.push(<a href={urls[i]}>{sitenames[i]}</a>);
+        sites.push(<a href={urls[i]} key={i}>{sitenames[i]}</a>);
         if (i != (sitenames.length - 1)) {
 
         }
