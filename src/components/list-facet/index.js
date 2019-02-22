2be7abed8dd2e1fafc97454a850144cbf78b11a5
Jes Constantine <jesconstantine@gmail.com>
2019-02-22 08:08:24 -0500

    root-198: code cleanup

--- src/components/list-facet/index.js
+++ src/components/list-facet/index.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
-import React from "react";
-import cx from "classnames";
-import queryString from "query-string";
+import React from 'react';
+import cx from 'classnames';
+import queryString from 'query-string';
 import AnimateHeight from 'react-animate-height';
 
 
@@ -11,22 +11,22 @@ class FederatedListFacet extends React.Component {
     super(props);
 
     this.state = {
-      filter: "",
-      truncateFacetListsAt: props.truncateFacetListsAt
+      filter: '',
+      truncateFacetListsAt: props.truncateFacetListsAt,
     };
   }
 
   handleClick(value) {
     const foundIdx = this.props.value.indexOf(value);
     // Get existing querystring params.
-    let parsed = queryString.parse(window.location.search);
+    const parsed = queryString.parse(window.location.search);
 
     // Those filter fields for which we want to preserve state in qs.
     // @todo handle parsing of terms and dates
     // @todo store this in app config?
     const filterFieldsWithQsState = [
-      "sm_site_name",
-      "ss_federated_type"
+      'sm_site_name',
+      'ss_federated_type',
     ];
 
     const isQsParamField = filterFieldsWithQsState.find((item) => item === this.props.field);
@@ -39,8 +39,7 @@ class FederatedListFacet extends React.Component {
 
       // Send new query based on app state.
       this.props.onChange(this.props.field, this.props.value.concat(value));
-    }
-    else {
+    } else {
       if (isQsParamField) {
         // Remove the param for this field from the parsed qs object.
         delete parsed[this.props.field];
@@ -56,11 +55,9 @@ class FederatedListFacet extends React.Component {
       // Update the querystring params in the browser, add path to history.
       // See: https://developer.mozilla.org/en-US/docs/Web/API/History_API#The_pushState()_method
       if (window.history.pushState) {
-        const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + stringified;
-        window.history.pushState({path: newurl}, '', newurl);
-        console.log(newurl)
-      }
-      else {
+        const newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${stringified}`;
+        window.history.pushState({ path: newurl }, '', newurl);
+      } else {
         window.location.search = stringified;
       }
     }
@@ -71,12 +68,12 @@ class FederatedListFacet extends React.Component {
     // If this is a hierarchical list facet.
     if (hierarchyFacetValue) {
       // Determine the current state of the expanded hierarchical list facets.
-      const indexOfExpandedHierarchyFacetValue = this.props.expandedHierarchies.indexOf(hierarchyFacetValue);
+      const indexOfExpandedHierarchyFacetValue = this.props.expandedHierarchies
+        .indexOf(hierarchyFacetValue);
       if (indexOfExpandedHierarchyFacetValue > -1) {
         // This accordion is currently expanded, so collapse it.
         this.props.expandedHierarchies.splice(indexOfExpandedHierarchyFacetValue,1);
-      }
-      else {
+      } else {
         // This accordion is currently collapsed, so expand it.
         this.props.expandedHierarchies.push(hierarchyFacetValue);
       }
@@ -84,14 +81,21 @@ class FederatedListFacet extends React.Component {
   }
 
   render() {
-    const { label, facets, field, value, collapse, hierarchy } = this.props;
+    const {
+      label,
+      facets,
+      field,
+      value,
+      collapse,
+      hierarchy,
+    } = this.props;
     const { truncateFacetListsAt } = this.state;
 
     const facetCounts = facets.filter((facet, i) => i % 2 === 1);
     const facetValues = facets.filter((facet, i) => i % 2 === 0);
     // Create an object of facets {value: count} to keep consistent for inputs.
     const facetInputs = {};
-    facetValues.forEach((value, i) => {
+    facetValues.forEach((v, i) => {
       const key = facetValues[i];
       facetInputs[key] = facetCounts[i];
     });
@@ -99,7 +103,8 @@ class FederatedListFacet extends React.Component {
     const expanded = !(collapse || false);
     const height = expanded ? 'auto' : 0;
 
-    // If we need to generate multiple list-fact accordion groups from this list-facet field (i.e. sm_federated_terms).
+    // If we need to generate multiple list-fact accordion groups
+    // from this list-facet field (i.e. sm_federated_terms).
     if (hierarchy) {
       // Iterate through sm_federated_terms array of values.
       // Each value is a string with the format Type>Term.
@@ -124,60 +129,72 @@ class FederatedListFacet extends React.Component {
         // If we don't already have terms.Type then create it.
         if (!Object.hasOwnProperty.call(terms, pieces[0])) {
           terms[pieces[0]] = {};
-          terms[pieces[0]]['items'] = [];
-          terms[pieces[0]]['expanded'] = (this.props.expandedHierarchies.indexOf(pieces[0]) > -1);
-          terms[pieces[0]]['height'] = terms[pieces[0]]['expanded'] ? 'auto' : 0;
+          terms[pieces[0]].items = [];
+          terms[pieces[0]].expanded = (this.props.expandedHierarchies.indexOf(pieces[0]) > -1);
+          terms[pieces[0]].height = terms[pieces[0]].expanded ? 'auto' : 0;
         }
         // Add the object for this facet value to the array of terms for this type.
-        terms[pieces[0]]['items'].push({term: pieces[1], facetValue: facetValue, facetCount: facetCounts[i]});
+        terms[pieces[0]].items.push({
+          term: pieces[1],
+          facetValue,
+          facetCount: facetCounts[i],
+        });
       });
 
       // Remove duplicate types
       // So facet values of "Condition>Bones", "Condition>Bone growth" should only
       // Add "Condition" type once so we only render 1 Condition accordion group.
-      const uniqueTypes = types.filter((value, index, self) => self.indexOf(value) === index).filter(String);
+      const uniqueTypes = types.filter((v, i, self) => self.indexOf(v) === i).filter(String);
 
       // Define array of accordion Lis which we'll populate with react fragments.
-      let listFacetHierarchyLis = [];
+      const listFacetHierarchyLis = [];
       // Define array of checkbox Lis which we'll populate with react fragments, per type.
-      let listFacetHierarchyTermsLis = [];
+      const listFacetHierarchyTermsLis = [];
       // Iterate through types (accordion lis).
       uniqueTypes.forEach((type, i) => {
         // Populate the checkbox lis react fragments for each type.
         listFacetHierarchyTermsLis[type] = [];
-        terms[type]['items'].forEach((termObj, i) => termObj.facetCount && listFacetHierarchyTermsLis[type].push(
-          <li key={`${termObj.term}_${termObj.facetValue}_${i}`}>
+        terms[type].items.forEach((termObj, i) => termObj.facetCount
+          && listFacetHierarchyTermsLis[type].push(<li key={`${termObj.term}_${termObj.facetValue}_${i}`}>
             <label className="search-accordion__checkbox-label">
             <input
-                type="checkbox"
-                name={type}
-                value={termObj.facetValue}
-                checked={value.indexOf(termObj.facetValue) > -1}
-                onChange={() => this.handleClick(termObj.facetValue)}
+              type="checkbox"
+              name={type}
+              value={termObj.facetValue}
+              checked={value.indexOf(termObj.facetValue) > -1}
+              onChange={() => this.handleClick(termObj.facetValue)}
             /> {termObj.term}
-            <span className="facet-item-amount"> ({termObj.facetCount})</span>
-          </label>
-        </li>));
+              <span className="facet-item-amount"> ({termObj.facetCount}
+                <span className="element-invisible">results</span>)
+              </span>
+            </label>
+          </li>));
 
         // Populate the accordion lis array with all of its checkboxes.
         listFacetHierarchyTermsLis[type].length && listFacetHierarchyLis.push(
           <li id={`solr-list-facet-${type}`} key={`solr-list-facet-${type}-${i}`}>
             <a
               tabIndex="0"
-              className={cx("search-accordion__title", {"js-search-accordion-open": terms[type]['expanded']})}
+              className={cx('search-accordion__title', { 'js-search-accordion-open': terms[type].expanded })}
               id={label.replace(/\s+/g, '-').toLowerCase()}
               onClick={this.toggleExpand.bind(this, type)}
-            >{type}</a>
+              onKeyDown={(event) => {
+                if (event.keyCode === 13) {
+                  this.toggleExpand(type);
+                }
+              }}
+            >
+              <span className="element-invisible">Toggle filter group for</span> {type}
+            </a>
             <AnimateHeight
               duration={600}
-              height={terms[type]['height']}
+              height={terms[type].height}
             >
               <ul className="search-accordion__content" key={`solr-list-facet-${type}-ul`}>
                 {listFacetHierarchyTermsLis[type]}
               </ul>
             </AnimateHeight>
-          </li>
-        );
+          </li>);
       });
       // Render the group of accordion lis with their facet value checkbox lists.
       return listFacetHierarchyLis;
@@ -187,17 +204,23 @@ class FederatedListFacet extends React.Component {
     return (
       <li id={`solr-list-facet-${field}`}>
         <a
-            tabIndex="0"
-            className={cx("search-accordion__title", {"js-search-accordion-open": expanded})}
-            id={label.replace(/\s+/g, '-').toLowerCase()}
-            onClick={this.toggleExpand.bind(this)}
+          tabIndex="0"
+          className={cx('search-accordion__title', { 'js-search-accordion-open': expanded })}
+          id={label.replace(/\s+/g, '-').toLowerCase()}
+          onClick={this.toggleExpand.bind(this)}
         >{label}</a>
         <AnimateHeight
           duration={600}
           height={height}
         >
           <ul className="search-accordion__content" key={`solr-list-facet-${field}-ul`}>
-            {facetValues.filter((facetValue, i) => facetInputs[facetValue] > 0 && (truncateFacetListsAt < 0 || i < truncateFacetListsAt)).map((facetValue, i) => this.state.filter.length === 0 || facetValue.toLowerCase().indexOf(this.state.filter.toLowerCase()) > -1 ? (<li key={`${facetValue}_${facetInputs[facetValue]}`}>
+            {facetValues.filter((facetValue, i) => facetInputs[facetValue] > 0
+                && (truncateFacetListsAt < 0 || i < truncateFacetListsAt))
+              .map((facetValue, i) => {
+                if (this.state.filter.length === 0
+                  || facetValue.toLowerCase().indexOf(this.state.filter.toLowerCase()) > -1) {
+                  return (
+                    <li key={`${facetValue}_${facetInputs[facetValue]}`}>
               <label className="search-accordion__checkbox-label">
               <input
                 type="checkbox"
@@ -208,7 +231,11 @@ class FederatedListFacet extends React.Component {
               /> {facetValue}
               <span className="facet-item-amount"> ({facetInputs[facetValue]})</span>
             </label>
-            </li>) : null)}
+                    </li>
+                  );
+                }
+                return null;
+              })}
           </ul>
         </AnimateHeight>
       </li>
@@ -219,7 +246,7 @@ class FederatedListFacet extends React.Component {
 FederatedListFacet.defaultProps = {
   hierarchy: false,
   expandedHierarchies: [],
-  value: []
+  value: [],
 };
 
 FederatedListFacet.propTypes = {
@@ -237,7 +264,7 @@ FederatedListFacet.propTypes = {
   onSetCollapse: PropTypes.func,
   query: PropTypes.object,
   truncateFacetListsAt: PropTypes.number,
-  value: PropTypes.array
+  value: PropTypes.array,
 };
 
 export default FederatedListFacet;
