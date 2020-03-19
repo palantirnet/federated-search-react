3187d15dff2a225f604b1d04bc76f52db19b6415
Russom Woldezghi <woldezghi@palantir.net>
2020-03-19 11:41:51 -0400

    Adds default testing framework for federated

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,7 +1,27 @@
+// <reference types="cypress" />
 
 describe('App is loading', () => {
   it('Verify Federated Search App is loading basic markup.', () => {
     cy.visit('/');
-    cy.contains('Please enter a search term.');
+    cy.get('#fs-root');
+
+    // Text field search.
+    cy.contains('Enter search term:');
+    cy.get('.fs-search-form__input');
+    cy.get('.fs-search-form__submit');
+
+    // Sort by field.
+    cy.contains('Sort By');
+    cy.get('#sort-by');
+
+    // Fitler fields.
+    cy.contains('Filter Results');
+    cy.get('form.fs-search-filters__form').find('.fs-search-accordion');
+    cy.get('.fs-search-filters').find('.fs-search-accordion__group-item');
+    cy.get('.fs-search-filters').find('.fs-search-accordion__content-item');
+    cy.get('.fs-search-filters__reset').should('be.visible');
+
+    // Default message text when app is loaded.
+    cy.get('.solr-search-results-container__prompt').contains('Please enter a search term.');
   });
 });
