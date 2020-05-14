af192035a47ddade38fa87b412dee48ab867860f
Russom Woldezghi <woldezghi@palantir.net>
2020-05-14 11:45:00 -0400

    Do an exact match of results, copied from cypress error reporting

--- cypress/integration/keyword.test.js
+++ cypress/integration/keyword.test.js
@@ -17,6 +17,7 @@ describe('Keyword search', () => {
     cy.get('#stat').contains('Showing ' + resultCount + ' results');
     cy.get('.fs-search-results__heading')
       .should('have.length', resultCount);
+    cy.get('.fs-search-accordion__content-item').should('have.text',' Drupal 8 (2results) Drupal 8 - Three (1results) Search Drupal 7 (2results) Article (3results) Page (2results)-Toggle filter group for Age Adult (1results) Mature (2results) Puppy (2results) Adult (1results) Mature (2results) Puppy (2results)Toggle filter group for Color Brown (1results) Gold (1results) Yellow (1results) Brown (1results) Gold (1results) Yellow (1results)Toggle filter group for Traits Athletic (1results) Curious (1results) Energetic (1results) Loyal (3results) Athletic (1results) Curious (1results) Energetic (1results) Loyal (3results)');
 
     // Verify pager page link/count.
     cy.get('.fs-search-pager__item.is-active .fs-search-pager__item-button')
