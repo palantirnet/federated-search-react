08ecbd5309bb9d93098e27e317d86a71ed900d84
Byron Duvall <duvall@palantir.net>
2021-01-20 15:37:54 -0500

    fix eslint errors in FederatedCountLabel component

--- src/components/results/count-label.js
+++ src/components/results/count-label.js
@@ -9,17 +9,45 @@ function searchResultsStat(currentPage, numFound, rows, pageAmt, noResultsText, 
   if (numFound > rows) { // Many pages
     a11yMessage = `Showing page ${currentPage + 1} of ${pageAmt} (${numFound} results).`;
     message = (
-      <span>Showing page
-        <b> {currentPage + 1}</b> of
-        <b> {pageAmt}</b> (<b>{numFound}</b> results).
+      <span>
+        Showing page
+        <b>
+          {' '}
+          {currentPage + 1}
+        </b>
+        {' '}
+        of
+        <b>
+          {' '}
+          {pageAmt}
+        </b>
+        {' '}
+        (
+        <b>{numFound}</b>
+        {' '}
+        results).
       </span>
     );
   } else if (numFound <= rows && numFound > 1) { // Single page
     a11yMessage = `Showing ${numFound} results.`;
-    message = (<span>Showing <b>{numFound}</b> results.</span>);
+    message = (
+      <span>
+        Showing
+        <b>{numFound}</b>
+        {' '}
+        results.
+      </span>
+    );
   } else if (numFound === 1) { // Single item
     a11yMessage = `Showing ${numFound} result.`;
-    message = (<span>Showing <b>{numFound}</b> result.</span>);
+    message = (
+      <span>
+        Showing
+        <b>{numFound}</b>
+        {' '}
+        result.
+      </span>
+    );
   } else if (numFound === 0) { // No results
     message = noResultsText || 'Sorry, your search yielded no results.';
     a11yMessage = message;
@@ -27,40 +55,40 @@ function searchResultsStat(currentPage, numFound, rows, pageAmt, noResultsText, 
   // Don't announce total results when wildcard query sent on term clear.
   a11yMessage = termValue ? a11yMessage : '';
   return (
-    <React.Fragment>
+    <>
       <LiveMessage message={a11yMessage} aria-live="polite" />
       <p id="stat" tabIndex="-1" className="fs-search-results-stat">{message}</p>
-    </React.Fragment>
+    </>
   );
 }
 
-class FederatedCountLabel extends React.Component {
-  render() {
-    const {
-      numFound,
-      start,
-      rows,
-      noResultsText,
-      termValue,
-    } = this.props;
-    const currentPage = start / rows;
-    const pageAmt = Math.ceil(numFound / rows);
-    return (
-      <React.Fragment>
-        {searchResultsStat(currentPage, numFound, rows, pageAmt, noResultsText, termValue)}
-      </React.Fragment>
-    );
-  }
-}
+const FederatedCountLabel = function ({
+  numFound,
+  start,
+  rows,
+  noResultsText,
+  termValue,
+}) {
+  const currentPage = start / rows;
+  const pageAmt = Math.ceil(numFound / rows);
+  return (
+    <>
+      {searchResultsStat(currentPage, numFound, rows, pageAmt, noResultsText, termValue)}
+    </>
+  );
+};
 
 FederatedCountLabel.propTypes = {
   numFound: PropTypes.number.isRequired,
   start: PropTypes.number.isRequired,
-  rows: PropTypes.number,
+  rows: PropTypes.number.isRequired,
+  noResultsText: PropTypes.string,
+  termValue: PropTypes.string,
 };
 
 FederatedCountLabel.defaultProps = {
-  start: 0,
+  noResultsText: '',
+  termValue: '',
 };
 
 export default FederatedCountLabel;
