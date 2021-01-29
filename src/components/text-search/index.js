9e96696570f3eb9a7e3fb65401a00e20d73295a7
Byron Duvall <duvall@palantir.net>
2021-01-29 08:43:09 -0500

    fix eslint errors in FederatedTextSearch component

--- src/components/text-search/index.js
+++ src/components/text-search/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FederatedTextSearchNoAutocomplete from './no-autocomplete';
 import FederatedTextSearchAsYouType from './search-as-you-type';
 
@@ -7,7 +8,16 @@ const FederatedTextSearch = (props) => {
   const InputComponent = autocomplete
     ? FederatedTextSearchAsYouType
     : FederatedTextSearchNoAutocomplete;
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
   return <InputComponent {...props} />;
+};
+
+FederatedTextSearch.defaultProps = {
+  autocomplete: false,
+};
+
+FederatedTextSearch.propTypes = {
+  autocomplete: PropTypes.bool,
 };
 
 export default FederatedTextSearch;
